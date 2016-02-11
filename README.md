e3a49546b993ba97ec92090a1e5123bb287b696f
Chafouin <tech@commeunarbre.fr>
2016-02-11 15:46:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Monitaure
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/ServersController.js
@@ -0,0 +1,11 @@
+/**
+ * ServersController
+ *
+ * @description :: Server-side logic for managing Servers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
