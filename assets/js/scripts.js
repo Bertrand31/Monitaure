58550a484c3dd324e94fe24f69cee792607a1def
Chafouin <tech@commeunarbre.fr>
2016-02-18 16:46:11 +0100

    Front-end cleanup

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -52,7 +52,7 @@ $(document).ready(function() {
     socket.on('checksData', function(data) {
         for(i = 0; i < data.length; i++) {
             var target = $('tr#' + data[i].id);
-            target.find('td.response-time').text(data[i].duration);
+            target.find('td.response-time').text(data[i].duration + 'ms');
             target.find('td.status').text(data[i].open ? 'OK' : 'NOK');
         }
     });
