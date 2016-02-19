5001a3cddafe977485d8ce535c875216ad956e6a
Chafouin <tech@commeunarbre.fr>
2016-02-19 15:53:47 +0100

    Turned independent worker into a regular service

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -12,9 +12,9 @@
 module.exports.bootstrap = function(cb) {
 
     sails.on('lifted', function() {
-        var worker  = require(__dirname + '/../worker.js');
+        // var worker  = require(__dirname + '/../worker.js');
         setInterval(function() {
-            worker(function(data) {
+            Worker(function(data) {
                 sails.sockets.blast('checksData', data);
             });
         },5000);
