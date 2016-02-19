8d462ad46b7dae13f2cd3809aafd5c3c5b2abaa9
Bertrand31 <bertrandjun@gmail.com>
2016-02-19 23:06:43 +0100

    Added Chart.js & Moment JS

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -28,6 +28,9 @@ var jsFilesToInject = [
   // Load sails.io before everything else
   'js/dependencies/sails.io.js',
 
+  'js/dependencies/moment.min.js',
+  'js/dependencies/Chart.min.js',
+
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
 
