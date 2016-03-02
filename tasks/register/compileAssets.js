251db59456b2464fd6d88094c835169d21e22ab3
Bertrand31 <bertrandjun@gmail.com>
2016-03-02 23:07:19 +0100

    Temporarily disabled clean:dev Grunt task

--- tasks/register/compileAssets.js
+++ tasks/register/compileAssets.js
@@ -12,11 +12,11 @@
  *
  */
 module.exports = function(grunt) {
-  grunt.registerTask('compileAssets', [
-    'clean:dev',
-    'jst:dev',
-    'sass:dev',
-    'copy:dev',
-    'coffee:dev'
-  ]);
+    grunt.registerTask('compileAssets', [
+        // 'clean:dev',
+        'jst:dev',
+        'sass:dev',
+        'copy:dev',
+        'coffee:dev'
+    ]);
 };
