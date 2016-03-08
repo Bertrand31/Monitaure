ec72c4db312f34a726801be5afaf9a408db76b89
Bertrand31 <bertrandjun@gmail.com>
2016-03-08 21:38:46 +0100

    Created user model

---
+++ api/models/Users.js
@@ -0,0 +1,34 @@
+var bcrypt = require('bcrypt');
+
+module.exports = {
+    attributes: {
+        email: {
+            type: 'email',
+            required: true,
+            unique: true
+        },
+        password: {
+            type: 'string',
+            minLength: 6,
+            required: true
+        },
+        toJSON: function() {
+            var obj = this.toObject();
+            delete obj.password;
+            return obj;
+        }
+    },
+    beforeCreate: function(user, cb) {
+        bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) {
+                    console.log(err);
+                    cb(err);
+                } else {
+                    user.password = hash;
+                    cb();
+                }
+            });
+        });
+    }
+};
