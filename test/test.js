dd3e6c4392441a394fe75cb6adddfeacd922fd0d
Bertrand31 <bertrandjun@gmail.com>
2016-04-03 23:20:36 +0200

    Updated Mocha tests

--- test/test.js
+++ test/test.js
@@ -1,41 +1,59 @@
 var sails = require('sails');
 var assert = require('assert');
+var request = require('supertest');
 
 before(function(done) {
-	// Increase the Mocha timeout so that Sails has enough time to lift.
-	this.timeout(5000);
-	sails.lift({
-		// configuration for testing purposes
-	}, function(err, server) {
-		if (err) return done(err);
-		// here you can load fixtures, etc.
-		done(err, sails);
-	});
+    // Increase the Mocha timeout so that Sails has enough time to lift.
+    this.timeout(5000);
+    sails.lift({
+        // configuration for testing purposes
+    }, function(err, server) {
+        if (err) return done(err);
+        // here you can load fixtures, etc.
+        done(err, sails);
+    });
 });
 
 after(function(done) {
-	// here you can clear fixtures, etc.
-	sails.lower(done);
+    // here you can clear fixtures, etc.
+    sails.lower(done);
 });
 
+describe('UserModel', function() {
 
-describe('Checks management', function() {
+    describe('#find()', function() {
+        it('should check find function', function (done) {
+            User.find()
+                .then(function(results) {
+                    // Tests
+                    done();
+                })
+            .catch(done);
+        });
+    });
 
-	var sampleCheck = {
-        name: 'HTTP @ Portfolio',
-		domainNameOrIP: 'awebsiteabout.me',
-        port: 80
-	};
-	describe('Create a check', function () {
-		it('should return the created check', function () {
-            ChecksManagement.createCheck(sampleCheck, function(created) {
-                // test.string(created.name).isIdenticalTo('HTTP @ SYS');
-                // test.object(created).hasKey('name', 'HTTP @ SYS');
-                // done();
-                var err = true;
-                if (err) return done(err);
-                done();
-			});
-		});
-	});
 });
+
+describe('UserController', function() {
+
+    describe('#signup()', function() {
+        it('should create an user', function (done) {
+            request(sails.hooks.http.app)
+                .post('/signup')
+                .send({ name: 'TestUser', email: 'testuser@test.com', password: 'testpassword', confirmPassword: 'testpassword' })
+                .expect(200);
+            done();
+        });
+    });
+    describe('#login()', function() {
+        it('should redirect to /dashboard', function (done) {
+            request(sails.hooks.http.app)
+                .post('/login')
+                .send({ name: 'TestUser', password: 'testpassword' })
+                .expect(200);
+            done();
+        });
+    });
+
+});
+
