2d8b8e2d528e578e3acb2c014a958a2c49471e35
Bertrand31 <bertrandjun@gmail.com>
2016-04-19 20:51:08 +0200

    Added email alerts throught nodemailer and sendgrid API

---
+++ api/services/Notifications.js
@@ -0,0 +1,25 @@
+var nodemailer = require('nodemailer');
+var sgTransport = require('nodemailer-sendgrid-transport');
+
+var sendgridOptions = {
+    auth: {
+        api_user: sails.config.sendgridUsername,
+        api_key: sails.config.sendgridPassword
+    }
+};
+
+var emailClient = nodemailer.createTransport(sgTransport(sendgridOptions));
+
+module.exports = {
+    sendEmailAlert: function(recipient, checkName, callback) {
+        var email = {
+            from: sails.config.emailAddress,
+            to: recipient,
+            subject: 'Monitaure alert: ' + checkName + ' is down!',
+            text: 'Alert: ' + checkName + ' is down'
+        };
+        emailClient.sendMail(email, function(err, info) {
+            if (err) console.log(err);
+        });
+    }
+};
--- api/services/Worker.js
+++ api/services/Worker.js
@@ -2,56 +2,64 @@ var async = require('async');
 var net = require('net');
 
 var checkPort = function(check, callback) {
-    var dateStart = new Date();
-    var timeStart = Date.now();
+	var dateStart = new Date();
+	var timeStart = Date.now();
 
-    var callbackObject = {
-        checkId: check.id,
-        checkName: check.name,
-        open: false,
-        duration: null,
-        date: dateStart
-    };
+	var callbackObject = {
+		checkId: check.id,
+		checkName: check.name,
+		checkEmailNotifications: check.emailNotifications,
+		checkOwner: check.owner,
+		open: false,
+		duration: null,
+		date: dateStart
+	};
 
-    var connection = net.connect(check.port, check.domainNameOrIP, function() {
-        var difference = Date.now() - timeStart;
-        connection.destroy();
-        callbackObject.open = true;
-        callbackObject.duration = difference;
-        return callback(callbackObject);
-    });
-    connection.on('error', function() {
-        return callback(callbackObject);
-    });
-    setTimeout(function() {
-        if (!connection.destroyed) {
-            connection.destroy();
-            return callback(callbackObject);
-        }
-    }, sails.config.checkTimeout);
+	var connection = net.connect(check.port, check.domainNameOrIP, function() {
+		var difference = Date.now() - timeStart;
+		connection.destroy();
+		callbackObject.open = true;
+		callbackObject.duration = difference;
+		return callback(callbackObject);
+	});
+	connection.on('error', function() {
+		return callback(callbackObject);
+	});
+	setTimeout(function() {
+		if (!connection.destroyed) {
+			connection.destroy();
+			return callback(callbackObject);
+		}
+	}, sails.config.checkTimeout);
 };
 
 module.exports = function () {
-    setInterval(function() {
-        Check.find().exec(function(err, checks) {
-            if (err) throw err;
+	setInterval(function() {
+		Check.find().exec(function(err, checks) {
+			if (err) throw err;
 
-            var asyncChecks = [];
+			var asyncChecks = [];
 
-            checks.forEach(function(check) {
-                asyncChecks.push(function(callback) {
-                    checkPort(check, function(result){
-                        callback(null, result);
-                    });
-                });
-            });
+			checks.forEach(function(check) {
+				asyncChecks.push(function(callback) {
+					checkPort(check, function(result){
+						callback(null, result);
+					});
+				});
+			});
 
-            async.parallel(asyncChecks, function(err, pings){
-                if (err) throw err;
-                pings.forEach(function(ping) {
-                    CheckManagement.insertHistory(ping);
-                });
-            });
-        });
-    }, sails.config.checkInterval);
+			async.parallel(asyncChecks, function(err, pings){
+				if (err) throw err;
+
+				pings.forEach(function(ping) {
+					CheckManagement.insertHistory(ping);
+					if (ping.checkEmailNotifications && !ping.open) {
+						User.findOne({id: ping.checkOwner}).exec(function(err, user) {
+							Notifications.sendEmailAlert(user.email, ping.checkName);
+						});
+					}
+				});
+			});
+		});
+	}, sails.config.checkInterval);
 };
