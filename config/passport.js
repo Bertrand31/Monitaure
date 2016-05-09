a41315d41fe0842d667f29eb902d2655dde7538e
Bertrand31 <bertrandjun@gmail.com>
2016-05-09 23:08:34 +0200

    Fix the 'failed to deserialize user' bug + various code imrovements

--- config/passport.js
+++ config/passport.js
@@ -2,14 +2,17 @@ var passport = require('passport'),
 	LocalStrategy = require('passport-local').Strategy,
 	bcrypt = require('bcrypt');
 
-passport.serializeUser(function(user, done) {
-	done(null, user.id);
+passport.serializeUser(function(user, callback) {
+	return callback(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
+passport.deserializeUser(function(id, callback) {
 	User.findOne({ id: id }, function (err, user) {
-		done(err, user);
-		// done(null, user);
+        if (!user) {
+            return callback(null, false);
+        } else {
+            return callback(null, user);
+        }
 	});
 });
 
@@ -17,21 +20,21 @@ passport.use(new LocalStrategy({
 		usernameField: 'username',
 		passwordField: 'password'
 	},
-	function(username, password, done) {
+	function(username, password, callback) {
 		User.findOne({ username: username }, function (err, user) {
-            if (err) return done(err);
+            if (err) return callback(err);
 
 			if (!user) {
-				return done(null, false, { message: 'Incorrect username.' });
+				return callback(null, false, { message: 'Incorrect username.' });
 			} else if (!user.confirmedAccount) {
-                return done(null, false, { message: 'Account not confirmed yet. Check your emails.' });
+                return callback(null, false, { message: 'Account not confirmed yet. Check your emails.' });
             }
 
 			bcrypt.compare(password, user.password, function (err, res) {
                 if (err) throw err;
 
 				if (!res)
-					return done(null, false, {
+					return callback(null, false, {
 						message: 'Invalid Password'
 					});
 				var returnUser = {
@@ -40,7 +43,7 @@ passport.use(new LocalStrategy({
 					createdAt: user.createdAt,
 					id: user.id
 				};
-				return done(null, returnUser, {
+				return callback(null, returnUser, {
 					message: 'Logged In Successfully'
 				});
 			});
