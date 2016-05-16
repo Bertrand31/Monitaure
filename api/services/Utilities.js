de3de50b7747423abb64abc6706023dbc4e12eaa
Bertrand31 <bertrandjun@gmail.com>
2016-05-16 19:14:02 +0200

    Fixed a bug when creating a check with an IP address

--- api/services/Utilities.js
+++ api/services/Utilities.js
@@ -14,7 +14,7 @@ module.exports = {
         const domainNameRegex = /^(?!:\/\/)([a-zA-Z0-9]+\.)?[a-zA-Z0-9][a-zA-Z0-9-]+\.[a-zA-Z]{2,6}?$/i,
             ipAddressRegex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/i;
 
-        return domainNameRegex.test(domainNameOrIP) || ipAddressRegex.test(domainNameOrIp);
+        return domainNameRegex.test(domainNameOrIP) || ipAddressRegex.test(domainNameOrIP);
     }
 
 };
