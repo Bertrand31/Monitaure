28b24e5f76f83a2da7821d2217811f91bbee4920
Bertrand31 <bertrandjun@gmail.com>
2016-05-26 22:35:03 +0200

    Added HTML5 attributes to forms (autofocus & maxlength)

--- assets/js/components/OverlayForm.react.js
+++ assets/js/components/OverlayForm.react.js
@@ -38,16 +38,16 @@ define(['react', '../actions/ChecksActions', '../stores/ChecksStore'],
                             <form OnSubmit={this._onCreateSubmit}>
                                 <fieldset className="check-data">
                                     <label for="name">Check name</label>
-                                    <input type="text" id="name" name="name" placeholder="e.g. HTTP @ Google.fr" value={this.state.selectedCheck.name} onChange={this.handleChnage} />
+                                    <input type="text" id="name" name="name" placeholder="e.g. HTTP @ Google.fr" value={this.state.selectedCheck.name} onChange={this._onChange} autofocus maxlength="20" />
                                     <label for="domainNameOrIP">Domain name or IP address</label>
-                                    <input type="text" id="domainNameOrIP" name="domainNameOrIP" placeholder="e.g. 216.58.211.99" value={this.state.selectedCheck.domainNameOrIP} onChange={this.handleChange} />
+                                    <input type="text" id="domainNameOrIP" name="domainNameOrIP" placeholder="e.g. 216.58.211.99" value={this.state.selectedCheck.domainNameOrIP} onChange={this._onChange} />
                                     <label for="port">Port number</label>
-                                    <input type="number" id="port" name="port" placeholder="e.g. 80" value={this.state.selectedCheck.port} onChange={this.handleChange}/>
+                                    <input type="number" id="port" name="port" placeholder="e.g. 80" value={this.state.selectedCheck.port} onChange={this._onChange} maxlength="5" />
                                 </fieldset>
                                 <fieldset className="notifications">
                                     <label className="checkbox-label" for="emailNotifications">Send email alerts?</label>
                                     <div className="rounded-checkbox">
-                                        <input id="emailNotifications" type="checkbox" name="emailNotifications" value={this.state.emailNotifications} onChange={this.handleChange} />
+                                        <input id="emailNotifications" type="checkbox" name="emailNotifications" value={this.state.emailNotifications} onChange={this._onChange} />
                                         <label htmlFor="emailNotifications"></label>
 
                                     </div>
