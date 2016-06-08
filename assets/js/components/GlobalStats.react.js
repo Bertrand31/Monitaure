e1ab799520895eacd66f31ca9ac6230febec1043
Bertrand31 <bertrandjun@gmail.com>
2016-06-08 09:51:44 +0200

    Global stats

--- assets/js/components/GlobalStats.react.js
+++ assets/js/components/GlobalStats.react.js
@@ -1,23 +1,39 @@
 define(['react'], function(React) {
-    return {
-        TotalChecks: React.createClass({
-            render: function() {
-                return (
-                    <span>
-                        {this.props.data.numberOfChecks}/{this.props.data.checksUp} servers
-                    </span>
-                    // <span class="secondary-text">are responding</span>
-                );
-            }
-        }),
-        AvailabilitiesAvg: React.createClass({
-            render: function() {
-                return (
-                    <span>
-                        {this.props.data.availabilitiesAvg}
-                    </span>
-                );
-            }
-        })
+    const GlobalStats = React.createClass({
+        render() {
+            return (
+                <div className="global-stats">
+                    <div className="global-data-block global-status">
+                        <div className="donut"></div>
+                        <p className="donut-content">
+                            <span>
+                                {this.props.data.numberOfChecks}/{this.props.data.checksUp} servers
+                            </span>
+                            // <span class="secondary-text">are responding</span>
+                        </p>
+                    </div>
+                    <div className="global-data-block availability">
+                        <div className="donut"></div>
+                        <p className="donut-content">
+                            <span>
+                                {this.props.data.availabilitiesAvg}
+                            </span>
+                        </p>
+                    </div>
+                    <div className="global-data-block last-error">
+                        <div className="donut"></div>
+                        <p className="donut-content">
+                            <span>
+                                <span>{this.props.data.lastError.checkName}</span>
+                                <span>{this.props.data.lastError.duration}</span>
+                            </span>
+                            <span className="aside-text">Last outage</span>
+                        </p>
+                    </div>
+                </div>
+            );
+        }
     };
+
+    return GlobalStats;
 });
