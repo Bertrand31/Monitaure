167ffb4597cb9a33788bd31f49a29888216d9426
Bertrand31 <bertrandjun@gmail.com>
2016-06-22 19:13:59 +0200

    Added title on top of the dashboard

--- assets/js/components/Dashboard.react.js
+++ assets/js/components/Dashboard.react.js
@@ -1,11 +1,11 @@
 define(
     ['react', 'react-dom',
-    './Dashboard/TopButton.react',
+    './Dashboard/TopBar.react',
     './Dashboard/GlobalStats.react',
     './Dashboard/CheckStats.react',
     './Dashboard/ChecksTable.react',
     '../actions/ChecksActions'],
-    function(React, ReactDOM, TopButton, GlobalStats, CheckStats, ChecksTable, ChecksActions) {
+    function(React, ReactDOM, TopBar, GlobalStats, CheckStats, ChecksTable, ChecksActions) {
 
         class Dashboard extends React.Component {
             constructor() {
@@ -21,7 +21,7 @@ define(
                 return (
                     <div>
                         <div className="pane-components-wrapper">
-                            <TopButton />
+                            <TopBar />
                             <GlobalStats />
                             <CheckStats />
                         </div>
