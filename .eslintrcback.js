addd21c066ab42670af93601a2989d33eda48e39
Bertrand31 <bertrandjun@gmail.com>
2016-08-01 18:49:30 +0200

    Added ESLint + Front & back configs + NPM scripts

---
+++ .eslintrcback.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4,
+            {"SwitchCase": 1}
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single",
+            {
+                "avoidEscape": true,
+                "allowTemplateLiterals": true
+            }
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-undef": 0,
+        "no-console": 0
+    }
+};
---
+++ .eslintrcfront.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": ["error", 4],
+    }
+};
