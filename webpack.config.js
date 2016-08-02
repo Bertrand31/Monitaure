3b586c9561127e3587dfa8b1c120b6c980807111
Bertrand31 <bertrandjun@gmail.com>
2016-08-02 19:13:23 +0200

    Added a 'build' script and split webpack's config into a 'dev' and a 'build' one

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-// const webpack = require('webpack');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
 
-module.exports = {
+const common = {
     context: __dirname,
     entry: ['whatwg-fetch', 'babel-polyfill', './assets/js/app.js'],
 
@@ -22,14 +23,28 @@ module.exports = {
             exclude: /node_modules/
         }]
     },
-    // plugins: [
-    //     new webpack.optimize.UglifyJsPlugin({
-    //         compress: {
-    //             // warnings: false,
-    //         },
-    //         output: {
-    //             comments: false,
-    //         },
-    //     }),
-    // ]
 };
+
+let config;
+
+switch (process.env.npm_lifecycle_event) {
+    case 'build':
+        config = merge(common, {
+            plugins: [
+                new webpack.optimize.UglifyJsPlugin({
+                    compress: {
+                        // warnings: false,
+                    },
+                    output: {
+                        comments: false,
+                    },
+                }),
+            ],
+        });
+        break;
+
+    default:
+        config = merge(common, {});
+}
+
+module.exports = config;
