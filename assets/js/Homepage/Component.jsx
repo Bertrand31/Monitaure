4c4a0c7668e3238f14ef239818b38cad7d9c0ca7
Bertrand31 <bertrandjun@gmail.com>
2016-09-06 21:12:12 +0200

    Fixed 'disappearing header' bug

--- assets/js/Homepage/Component.jsx
+++ assets/js/Homepage/Component.jsx
@@ -6,41 +6,43 @@ import SignupForm from '../User/Signup/Container';
 const HomepageComponent = ({ popover, open }) => (
     <div className="front" id="page-container">
         {popover}
-        <header className="header">
-            <h1 className="logo">
-                <img src="/images/logo.svg" width="264" height="39" alt="Monitaure - Monitoring for the masses" />
-            </h1>
-            <nav className="top-nav">
-                <ul>
-                    <li>
-                        <a href="#_">About us</a>
-                    </li>
-                    <li>
-                        <a href="#_">Contact</a>
-                    </li>
-                    <li>
+        <div className={`blur-container ${popover !== null ? 's-is-blurred' : ''}`}>
+            <header className="header">
+                <h1 className="logo">
+                    <img src="/images/logo.svg" width="264" height="39" alt="Monitaure - Monitoring for the masses" />
+                </h1>
+                <nav className="top-nav">
+                    <ul>
+                        <li>
+                            <a href="#_">About us</a>
+                        </li>
+                        <li>
+                            <a href="#_">Contact</a>
+                        </li>
+                        <li>
+                            <a
+                                href="/login"
+                                className="button button-empty button-round"
+                                onClick={(e) => { e.preventDefault(); open('login'); }}
+                            >Log in</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <main className="main">
+                <section className="hp-section desk-section">
+                    <div className="wrapper">
+                        <h2>Monitoring for the masses</h2>
+                        <h3>A simple and hassle-free server monitoring dashboard</h3>
                         <a
-                            href="/login"
-                            className="button button-empty button-round"
-                            onClick={(e) => { e.preventDefault(); open('login'); }}
-                        >Log in</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-        <main className={`main ${popover !== null ? 's-is-blurred' : ''}`}>
-            <section className="hp-section desk-section">
-                <div className="wrapper">
-                    <h2>Monitoring for the masses</h2>
-                    <h3>A simple and hassle-free server monitoring dashboard</h3>
-                    <a
-                        className="button button-round button-huge"
-                        href="/signup"
-                        onClick={(e) => { e.preventDefault(); open('signup'); }}
-                    >Sign up</a>
-                </div>
-            </section>
-        </main>
+                            className="button button-round button-huge"
+                            href="/signup"
+                            onClick={(e) => { e.preventDefault(); open('signup'); }}
+                        >Sign up</a>
+                    </div>
+                </section>
+            </main>
+        </div>
     </div>
 );
 
