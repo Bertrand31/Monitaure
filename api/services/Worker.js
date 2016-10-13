dffa4aea40b96abd05739f574d43f1e07ad3f1b1
Bertrand31 <bertrandjun@gmail.com>
2016-10-13 21:31:42 +0200

    Normalized DB functions inside Worker

--- api/services/Worker.js
+++ api/services/Worker.js
@@ -46,13 +46,13 @@ const pingHandling = (ping) => {
     if (ping.checkEmailNotifications && lastCheckHistory !== null) {
         // If the check is down and wasn't last time we checked
         if (!ping.open && lastCheckHistory.duration !== null) {
-            User.findOne({ id: ping.checkOwner }).exec((err, user) => {
+            DB.fetchOne('user', ping.checkOwner, (err, user) => {
                 if (err) sails.log.error(err);
                 Notifications.sendDownAlert(user, ping.checkId, ping.checkName);
             });
         // If the check is up and was down last time we checked
         } else if (ping.open && lastCheckHistory.duration === null) {
-            User.findOne({ id: ping.checkOwner }).exec((err, user) => {
+            DB.fetchOne('user', ping.checkOwner, (err, user) => {
                 if (err) sails.log.error(err);
 
                 let downtime = 0;
