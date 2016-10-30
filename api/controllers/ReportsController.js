97637a84a50eeaeda5fd12dc4c66d3e09bb832ff
Bertrand31 <bertrandjun@gmail.com>
2016-10-30 14:37:05 +0100

    Added a 'getall' route for Reports

---
+++ api/controllers/ReportsController.js
@@ -0,0 +1,21 @@
+module.exports = {
+    /**
+     * HTTP route to fetch all reports
+     * @param {Object} req - HTTP GET request
+     * @param {Object} res - Express' response object
+     * @returns {JSON} An error, if needs be, or the reports JSON object
+     */
+    getall: (req, res) => {
+        if (!req.wantsJSON) {
+            return res.forbidden();
+        }
+        Reports.getReports(DB.fetchOne, req.user.id, (err, reports) => {
+            if (err) return res.serverError(err);
+
+            Reports.markAsRead(reports);
+
+            return res.json(reports);
+        });
+    },
+};
+
