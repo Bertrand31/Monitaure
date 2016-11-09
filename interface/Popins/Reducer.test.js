f36db7354078debd70f89c511bfa662308c789d3
Bertrand31 <bertrandjun@gmail.com>
2016-11-09 22:15:00 +0100

    Added tests for Popins reducer

---
+++ interface/Popins/Reducer.test.js
@@ -0,0 +1,38 @@
+import * as types from './Constants';
+
+import reducer from './Reducer';
+
+describe('popinsReducer', () => {
+    it('should correctly add a popin', () => {
+        expect(
+            reducer(undefined, {
+                type: types.POPIN_CREATE,
+                id: 0,
+                variant: 'alert',
+                text: 'An error occured',
+            })
+        ).toEqual(
+            {
+                0: {
+                    id: 0,
+                    variant: 'alert',
+                    text: 'An error occured',
+                },
+            },
+        );
+    });
+    it('should correctly destroy a popin', () => {
+        expect(
+            reducer({
+                0: {
+                    id: 0,
+                    variant: 'alert',
+                    text: 'An error occured',
+                },
+            }, {
+                type: types.POPIN_DESTROY,
+                id: 0,
+            })
+        ).toEqual({});
+    });
+});
